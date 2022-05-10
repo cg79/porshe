7bd2dd2ff5d2a62441a361db7b803dcaec2b59ed
andreimihaitaTS <90057478+andreimihaitaTS@users.noreply.github.com>
2022-05-10 10:58:56 +0300

    navbar design partially completed

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,9 +1,23 @@
-// import styles from "./NavBar.modules.css";
+import styles from "./NavBar.module.css";
+import Logo from "./logo";
+// import "./navbar.css";
 
 const NavBar = (props: any) => {
   return (
     <>
-      <h1>da</h1>
+      <section className={styles.container}>
+        <nav className={styles.navbar}>
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+          <ul className={styles.items}>
+            <li>Overview</li>
+            <li>Companies</li>
+            <li>Support</li>
+            <li>Profile</li>
+          </ul>
+        </nav>
+      </section>
       {props.children}
     </>
   );

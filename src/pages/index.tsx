14e918837733601c5ad2e6121b751769710191bd
Claudiu Gombos <claudiu.gombos.extern@porsche.digital>
2022-05-12 16:38:10 +0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,70 @@
 // import { GetServerSideProps } from "next";
 import React from "react";
-import Link from "next/link"
-import { useSession, signIn, signOut } from 'next-auth/react';
-import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
-Amplify.configure(awsconfig)
+import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
+Amplify.configure(awsconfig);
 
+export default function Index() {
+  const { data: session, status } = useSession();
 
+  if (status === "loading") {
+    return null;
+  }
 
-export default function Index(){
-    const { data: session, status } = useSession()
+  const createUser = () => {
+    Amplify.Auth.signUp({
+      username: "claudiu.gombos@thinslices.com",
+      password: "Bufallo1234!",
+    })
+      .then((val: any) => {
+        console.log(val);
+        debugger;
+      })
+      .catch((err: any) => {
+        console.log(err);
+        debugger;
+      });
+  };
 
-    if(status==='loading'){
-        
-        return null;
-    }
+  const signIn1 = () => {
+    Amplify.Auth.signIn({
+      username: "claudiu.gombos@thinslices.com",
+      password: "Bufallo1234!",
+    })
+      .then((val: any) => {
+        console.log(val);
+        debugger;
+      })
+      .catch((err: any) => {
+        console.log(err);
+        debugger;
+      });
+  };
 
-    const signIn1=()=>{
-        Amplify.Auth.signUp({
-            username: 'claudiu',
-            password: 'Bufallo1234!'
-          })
-            .then((val: any) => {
-              console.log(val);
-              debugger;
-            })
-            .catch((err:any) => {
-              console.log(err);
-              debugger;
-            });
-    //     Amplify.Auth.signIn("asdad", "asdasdasd")
-    //   .then((val:any) => {
-    //     console.log(val);
-    //     debugger;
-    //   })
-    //   .catch((err:any) => {
-    //     console.log(err);
-    //     debugger;
-    //   });
-    }
-
-    if(session){
-        return (
-            <>
-              signed in as {JSON.stringify(session)}
-              <button onClick={()=>signOut()}>Sign Out</button>
-
-              <Link href='/companies' prefetch={false}>Companies</Link>
-            </>
-        )
-    }
-
+  if (session) {
     return (
-        <>
-        <button onClick={() => signIn1()}>Sign In</button>
-          <Link href='/companies' prefetch={false}>Companies</Link>
-        </>
-    )
-    
+      <>
+        signed in as {JSON.stringify(session)}
+        <button onClick={() => signOut()}>Sign Out</button>
+        <Link href="/companies" prefetch={false}>
+          Companies
+        </Link>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <button onClick={() => createUser()}>Create User</button>
+
+      <button onClick={() => signIn1()}>Sign In</button>
+      <Link href="/companies" prefetch={false}>
+        Companies
+      </Link>
+    </>
+  );
 }
 
-// export const getServerSideProps: GetServerSideProps = async function getServerSideProps(
-//   ctx
-// ) {
-//   let start = 0;
 
-//   if (ctx.query.start && typeof ctx.query.start === "string") {
-//     start = Number(ctx.query.start);
-//   }
-
-//   return {
-//     props: {
-//       hydrationData: {
-//         stopwatchStore: {
-//           start,
-//         },
-//       },
-//     },
-//   };
-// };

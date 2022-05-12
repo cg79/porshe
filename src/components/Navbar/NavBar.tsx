a667c940cf8e29db243574276beb317a09f8c903
Claudiu Gombos <claudiu.gombos.extern@porsche.digital>
2022-05-12 17:47:05 +0300

    before HUNTING after user profile

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,55 +1,64 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
-import Logo from './Logo'
-import styles from './NavBar.module.css'
-import NavBarButtons from './NavBarButtons'
-import { ROUTES } from './NavBarButtons'
-import MobileMenu from '../BurgerMenu/MobileMenu'
-import { useState, useEffect } from 'react'
+import Logo from "./Logo";
+import styles from "./NavBar.module.css";
+import NavBarButtons from "./NavBarButtons";
+import { ROUTES } from "./NavBarButtons";
+import MobileMenu from "../BurgerMenu/MobileMenu";
+import { useState, useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
+
+import Amplify from "aws-amplify";
+import awsconfig from "../../aws-exports";
+Amplify.configure(awsconfig);
+
 // import useWindowDimensions from '../../hooks/WindowDimension'
 
 export type ROUTE__INFO = {
-    url: string
-    name: string
-}
+  url: string;
+  name: string;
+};
 
 const NavBar = (props: any) => {
-    const router = useRouter()
+  const [width, setWidth] = useState<number>(1080);
+  const { data: session, status } = useSession();
 
-    const [width, setWidth] = useState<number>(1080)
+//   if (status === "loading") {
+//     return null;
+//   }
 
-    function handleWindowSizeChange() {
-        setWidth(window.innerWidth)
-    }
-    useEffect(() => {
-        setWidth(window.innerWidth)
+  const router = useRouter();
 
-        window.addEventListener('resize', handleWindowSizeChange)
-        return () => {
-            window.removeEventListener('resize', handleWindowSizeChange)
-        }
-    }, [])
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    setWidth(window.innerWidth);
 
-    return (
-        <>
-            {width <= 768 ? (
-                <MobileMenu />
-            ) : (
-                <section className={styles.container}>
-                    <nav className={styles.navbar}>
-                        <div className={styles.logo}>
-                            <Logo />
-                        </div>
-                        <ul className={styles.items}>
-                            {NavBarButtons(ROUTES, router)}
-                        </ul>
-                    </nav>
-                    <span>&nbsp;</span>
-                </section>
-            )}
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
 
-            {props.children}
-        </>
-    )
-}
-export default NavBar
+  return (
+    <>
+      {width <= 768 ? (
+        <MobileMenu />
+      ) : (
+        <section className={styles.container}>
+          <nav className={styles.navbar}>
+            <div className={styles.logo}>
+              <Logo />
+            </div>
+            <ul className={styles.items}>{NavBarButtons(ROUTES, router)}</ul>
+          </nav>
+          <span>&nbsp;</span>
+        </section>
+      )}
+
+      {props.children}
+    </>
+  );
+};
+export default NavBar;

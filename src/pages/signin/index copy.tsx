77e959989a148843fc2de6923953b4581c8b2e67
andreimihaitaTS <90057478+andreimihaitaTS@users.noreply.github.com>
2022-05-23 16:34:48 +0300

    safe commit

--- src/pages/signin/index copy.tsx
+++ src/pages/signin/index copy.tsx
@@ -3,14 +3,10 @@ import { LOADING_SVG, ROUTES } from "../../constants/constants";
 import Navbar from "../../components/Navbar";
 import ErrorMessage from "../../components/error/error";
 import { Auth } from "aws-amplify";
-import { Button, TextField, Box } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import Label from "../../components/label/label";
 import IdentityStore from "../../store/identity-store";
 import Router from "next/router";
-
-import styles from "./signin.module.css";
-import Logo from "../../components/Navbar/Logo";
-import { style } from "@mui/system";
 
 export default function SignIn(props: any) {
   if (props && props.porsche_user) {
@@ -19,10 +15,9 @@ export default function SignIn(props: any) {
 
   // const [cookie, setCookie] = useCookies(["user"])
 
-  const [email, setEmail] = useState("");
-  const [properEmail, setProperEmail] = useState(false);
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("claudiu9379@yahoo.com");
+  const [phone, setPhone] = useState("+40742917773");
+  const [password, setPassword] = useState("1111111a");
   const [errorMessage, setErrorMessage] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -32,9 +27,13 @@ export default function SignIn(props: any) {
     setEmail(newValue);
     setSubmitted(false);
     setErrorMessage("");
-    const isProper = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(newValue);
-    console.log(newValue.length, "debug");
-    setProperEmail(isProper);
+  };
+
+  const onPhoneChange = (event: any) => {
+    const newValue = event.target.value;
+    setPhone(newValue);
+    setSubmitted(false);
+    setErrorMessage("");
   };
 
   let onPasswordChange = (event: any) => {
@@ -75,11 +74,10 @@ export default function SignIn(props: any) {
     Auth.signIn(email, password)
       .then((data) => {
         debugger;
-        console.log("date", data);
 
         userHasBeenLogged({
-          name: "Remove_this_code",
-          email: "claudiu9379@yahoo.com",
+          name:'Remove_this_code',
+          email: 'claudiu9379@yahoo.com'
         });
         return;
 
@@ -120,66 +118,86 @@ export default function SignIn(props: any) {
   return (
     <div className="page-content">
       <Navbar>
-        <div className={styles.da}>
-          <Logo />
-          <Box
-            sx={{
-              height: 50,
-            }}
-          />
-          {/* <Label htmlFor="login" text="Login" /> */}
+        <div className="flex flex-column flex-center-y">
+          {/* <form name="form" onSubmit={triggerSignIn}> */}
 
-          <TextField
-            error={false}
-            helperText={
-              properEmail === false && email.length > 10
-                ? "enter a valid email adress"
-                : ""
-            }
-            label="Email"
-            variant="standard"
-            name="username"
-            value={email}
-            disabled={loading}
-            onChange={onEmailChange}
-            size={"small"}
-            sx={{
-              width: "16rem",
-              minWidth: "200px",
-            }}
-          />
-          <Box
-            sx={{
-              height: 50,
-            }}
-          />
-          <TextField
-            error={false}
-            helperText={""}
-            label="Password"
-            variant="standard"
-            name="password"
-            type={"password"}
-            value={password}
-            disabled={loading}
-            onChange={onPasswordChange}
-            sx={{
-              width: "16rem",
-              minWidth: "200px",
-            }}
-          />
-          <Box
-            sx={{
-              height: 50,
-            }}
-          />
-          <Button
-            variant="contained"
-            onClick={triggerSignIn}
-            disabled={!(email && password)}
-          >
-            Login
-          </Button>
+          <div className="flex">
+            <Label htmlFor="username" text="Email" />
+            <TextField
+              id="standard-basic"
+              label="Email"
+              variant="standard"
+              name="username"
+              value={email}
+              disabled={loading}
+              onChange={onEmailChange}
+            />
+
+            {submitted && !email && (
+              <div className="warning">Email is required</div>
+            )}
+          </div>
+
+          <div className="flex">
+            <Label htmlFor="username" text="Phone" />
+            <TextField
+              id="standard-basic"
+              label="Phone"
+              variant="standard"
+              name="phone"
+              value={phone}
+              disabled={loading}
+              onChange={onPhoneChange}
+            />
+
+            {submitted && !phone && (
+              <div className="warning">Phone is required</div>
+            )}
+          </div>
+
+          <div className="flex mt10">
+            <Label htmlFor="password" text="Password" />
+
+            {/* <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={password}
+              onChange={onPasswordChange}
+            /> */}
+            <TextField
+              id="standard-basic"
+              label="Password"
+              variant="standard"
+              name="password"
+              type={"password"}
+              value={password}
+              disabled={loading}
+              onChange={onPasswordChange}
+            />
+            {submitted && !password && (
+              <div className="warning">Password is required</div>
+            )}
+          </div>
+
+          <div className="mt10">
+            {/* <label className="lbl">&nbsp;</label> */}
+            <Button variant="contained" onClick={triggerSignIn}>
+              Login
+            </Button>
+
+            {loading && <img src={LOADING_SVG} />}
+          </div>
+
+          <div>
+            {/* <a href="/resetpassword">reset password</a> */}
+            <Button href="/forgotpassword">forgot password</Button>
+          </div>
+
+          <div className="flex flex-center mt10">
+            <ErrorMessage message={errorMessage}></ErrorMessage>
+          </div>
+          {/* </form> */}
         </div>
       </Navbar>
     </div>

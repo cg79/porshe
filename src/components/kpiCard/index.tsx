e938c512f7d1dd137dc037166cf48ce813e9f0e0
Mihăiță Andrei <mihaita.andrei@thinslices.com>
2022-05-27 14:01:15 +0300

    chart data properly displayed; some issues regarding the docs of chart js

---
+++ src/components/kpiCard/index.tsx
@@ -0,0 +1,21 @@
+import styles from './style.module.css'
+import { BarChartProps } from '../../components/data-types/data-types'
+import BarChart from '../../components/chart/bar-chart'
+
+import { Container } from '@mui/material'
+
+type KpiCardProps = {
+    title: string
+    chartData: BarChartProps
+    value: number
+}
+
+const KpiCard: React.FC<KpiCardProps> = ({ title, chartData, value }) => {
+    return (
+        <div className={styles.card}>
+            <BarChart props={chartData.props}></BarChart>
+        </div>
+    )
+}
+
+export default KpiCard
--- src/pages/kpi/financial-kpi.tsx
+++ src/pages/kpi/financial-kpi.tsx
@@ -1,30 +1,87 @@
-import React from "react";
-import BarChart from "../../components/chart/bar-chart";
-import { BarChartProps } from "../../components/data-types/data-types";
+import React from 'react'
+import BarChart from '../../components/chart/bar-chart'
+import { BarChartProps } from '../../components/data-types/data-types'
+const company_list = require('../../data/companies.json')
+import KpiCard from '../../components/kpiCard'
+
+const mapDataset = (metrics: any, index: number) => {
+    const titles: string[] = Object.keys(metrics)
+    const labels = []
+    const datasets: any[] = []
+    for (const [key, value] of Object.entries(metrics[titles[index]].data)) {
+        labels.push(key)
+        datasets.push(value)
+    }
+    const chartData = {
+        props: {
+            labels: labels,
+            datasets: [
+                {
+                    label: titles[index],
+                    data: datasets,
+                    backgroundColor: '#FFFFFF',
+                },
+            ],
+            value: metrics[titles[index]].value,
+            title: metrics[titles[index]].title,
+            // options: {
+            //     plugins: {
+            //         title: {
+            //             display: true,
+            //             text: 'Custom Chart Title',
+            //         },
+            //     },
+            // },
+        },
+    }
+    return chartData
+}
 
 export default function FinancialKpi(props: any) {
-  const defaultBarChartData: BarChartProps = {
-    props: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-          label: "Dataset 1",
-          data: [1, 2, 3],
-          backgroundColor: "rgba(255, 99, 132, 0.5)",
-        },
-        {
-          label: "Dataset 2",
-          data: [7, 8, 9],
-          backgroundColor: "rgba(53, 162, 235, 0.5)",
-        },
-      ],
-    },
-  };
+    // change the 0 to the selected company
+    const metricsChosen = company_list[0].kpis
 
-  return (
-    <div>
-      Financial Kpi
-      <BarChart props={defaultBarChartData.props}></BarChart>
-    </div>
-  );
+    const defaultBarChartData: BarChartProps = {
+        props: {
+            labels: [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+            ],
+            datasets: [
+                {
+                    label: 'Dataset 1',
+                    data: [1, 2, 3],
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+            ],
+        },
+    }
+
+    const barChartsData = Object.entries(metricsChosen).map(
+        ([key, value], index) => {
+            return mapDataset(metricsChosen, index)
+        }
+    )
+
+    const renderCharts = () => {
+        return barChartsData.map((chart): any => {
+            return (
+                <KpiCard
+                    title={chart.props.title}
+                    value={chart.props.value}
+                    chartData={chart}
+                />
+            )
+        })
+    }
+    return (
+        <>
+            <div className="company-container wrap">{renderCharts()}</div>
+        </>
+    )
 }

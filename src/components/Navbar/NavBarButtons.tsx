8ab931473c4e4fd27fa092a9cdad74bdaaac0cfb
Claudiu Gombos <claudiu.gombos.extern@porsche.digital>
2022-06-07 10:50:46 +0300

    hover menu items

--- src/components/Navbar/NavBarButtons.tsx
+++ src/components/Navbar/NavBarButtons.tsx
@@ -1,27 +1,30 @@
-import Link from 'next/link'
-import { NextRouter } from 'next/router'
-import styles from './NavBar.module.css'
+import Link from "next/link";
+import { NextRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
-import { ROUTE__INFO } from './NavBar'
+import { ROUTE__INFO } from "./NavBar";
 
 export const NAVIGATION_ROUTES: ROUTE__INFO[] = [
-    { url: '/overview', name: 'Overview' },
-    { url: '/companies', name: 'Companies' },
-    { url: '/support', name: 'Support' },
-]
+  { url: "/overview", name: "Overview" },
+  { url: "/companies", name: "Companies" },
+  { url: "/support", name: "Support" },
+];
 
 const NavBarButtons = (buttons: ROUTE__INFO[], router: NextRouter) => {
-    return buttons.map((button,index) => {
-        return (
-            <li key={index}
-                className={
-                    router.pathname == button.url ? styles.items__active : ''
-                }
-            >
-                <Link href={button.url}>{button.name}</Link>
-            </li>
-        )
-    })
-}
+  return buttons.map((button, index) => {
+    return (
+      <li
+        key={index}
+        className={router.pathname == button.url ? styles.items__active : ""}
+      >
+        <div className={styles.menuitem}>
+          <Link href={button.url}>
+            {button.name}
+          </Link>
+        </div>
+      </li>
+    );
+  });
+};
 
-export default NavBarButtons
+export default NavBarButtons;
